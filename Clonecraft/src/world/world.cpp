5c15b4b7b0609ca55713a0e4eb7bc7758551ef7d
natagoh <natagoh@stanford.edu>
2021-03-30 00:04:46 -0400

    world stores chunks in unordered_map

--- Clonecraft/src/world/world.cpp
+++ Clonecraft/src/world/world.cpp
@@ -1,21 +1,35 @@
 #include "world.h"
+#include <iostream>
 
 World::World() {
 	// prepare RENDER_DISTANCE * RENDER_DISTANCE chunks
-	chunks_to_render = new Chunk[RENDER_DISTANCE * RENDER_DISTANCE];
+	chunks_to_render = new glm::vec3[RENDER_DISTANCE * RENDER_DISTANCE];
 	for (int x = 0; x < RENDER_DISTANCE; x++) {
 		for (int z = 0; z < RENDER_DISTANCE; z++) {
 			// for now only add chunks horizontally
 			// todo: add vertical chunking
 			glm::vec3 position = glm::vec3(x * CHUNK_DIM, 0.0f, z * CHUNK_DIM);
-			chunks_to_render[x + RENDER_DISTANCE * z].setPosition(position);
-			chunks_to_render[x + RENDER_DISTANCE * z].generateMesh();
+			auto chunk = std::make_shared<Chunk>(position);
+			chunks.insert({ {position, chunk} });
+			chunks_to_render[x + RENDER_DISTANCE * z] = position;
+
+			// only need to generate meshes once since we don't change the mesh at all for now...
+			chunk->generateMesh();
 		}
 	}
 }
 
 void World::render() {
 	for (int i = 0; i < RENDER_DISTANCE * RENDER_DISTANCE; i++) {
-		chunks_to_render[i].render();
+		glm::vec3 chunk_coord = chunks_to_render[i];
+		
+		// find chunk in the unordered_map
+		std::unordered_map<glm::vec3, std::shared_ptr<Chunk>>::const_iterator map_item = chunks.find(chunk_coord);
+
+		if (map_item == chunks.end())
+			std::cout << "Error: (World) chunk not found" << std::endl;
+		else
+			map_item->second->render();
+
 	}
 }
--- Clonecraft/src/world/world.h
+++ Clonecraft/src/world/world.h
@@ -13,7 +13,7 @@ private:
 	const unsigned int RENDER_DISTANCE = 12;	// how many chunks can be seen on a screen
 
 	std::unordered_map<glm::vec3, std::shared_ptr<Chunk>> chunks;	// map containing all chunks
-	Chunk* chunks_to_render;	// array of chunks to be rendered
+	glm::vec3* chunks_to_render;	// array of chunks to be rendered
 
 public:
 	World();
